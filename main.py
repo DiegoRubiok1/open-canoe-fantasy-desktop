5c357474332a153da025d343f9394adb96d25422
DiegoRubiok1 <diegorubiocanales2006@gmail.com>
2025-05-17 17:33:12 +0200

    feat: first working kivy app

---
+++ main.py
@@ -0,0 +1,23 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager
+from screens.home_screen import HomeScreen
+from kivy.lang import Builder
+import os
+
+class OpenCanoeFantasyApp(App):
+    def build(self):
+        # Obtener la ruta del directorio actual
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        
+        # Construir la ruta completa al archivo kv
+        kv_path = os.path.join(current_dir, 'screens', 'home_screen.kv')
+        
+        # Cargar el archivo kv
+        Builder.load_file(kv_path)
+        
+        sm = ScreenManager()
+        sm.add_widget(HomeScreen(name='home'))
+        return sm
+
+if __name__ == '__main__':
+    OpenCanoeFantasyApp().run()
---
+++ screens/home_screen.py
@@ -0,0 +1,10 @@
+from kivy.uix.screenmanager import Screen
+
+class HomeScreen(Screen):
+    def go_to_players(self):
+        # Implementar navegación a la pantalla de jugadores
+        pass
+    
+    def go_to_teams(self):
+        # Implementar navegación a la pantalla de equipos
+        pass
